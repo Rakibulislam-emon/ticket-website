e38e19858bd1a4c78efef13972e271128f037fac
Rakibulislam-emon <rakibulislamemon60>
2024-02-20 17:46:52 +0600

    final commit

---
+++ jsdom/bus3.js
@@ -0,0 +1,7 @@
+function mmPrt(){
+    const nextbutton = document.getElementById('new')
+    nextbutton.classList.add('hidden')
+
+    const sucessPrt =document.getElementById('suc')
+    sucessPrt.classList.remove('hidden')
+}
